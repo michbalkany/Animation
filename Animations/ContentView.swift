a9bcbe736b47556abc936b76029da3f4d80a2d67
mich balkany <askmitchb@gmail.com>
2023-10-31 12:10:50 -0400

    applied the .overlay modifier and the .onAppear which will give the button an exterior circle with a pulse effect. deleted the .blur, .animation, and .easInOut modifiers.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,18 +11,26 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
         Button("Tap Me") {
-            animationAmount += 1
+//            animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(
-            .easeInOut(duration: 2)
-                .repeatForever(autoreverses: true),
-            value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+            )
+        .onAppear{
+            animationAmount = 2
+        }
     }
 }
 
