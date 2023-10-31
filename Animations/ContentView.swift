af9bd7b0e40e91750c1021980b3b377cf9f8ec2f
mich balkany <askmitchb@gmail.com>
2023-10-31 12:33:29 -0400

    created new animation that is dynamic with with two seperate views. each time stepper is clicked the button grows bigger, and prints out the size difference, everytime the button is tapped it grows bigger and prints out the size, and vice verca.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,26 +10,22 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        Button("Tap Me") {
-//            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-            )
-        .onAppear{
-            animationAmount = 2
+        
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale Amount", value: $animationAmount.animation(), in: (1...10))
+            
+            Spacer()
+            
+            Button("Tap Me"){
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
         }
     }
 }
